4658aa4cc42e2380bc3fa31dbb122031979ddd67
Виталий <bambrr@mail.ru>
2023-12-14 21:36:27 +0300

    Всё поменял к чертям собачим

--- H.W.28.py
+++ H.W.28.py
@@ -1,24 +1,26 @@
-import json
 import requests
-import pprint
+import json
+from marshmallow import Schema, fields, validate, ValidationError
+from marshmallow_dataclass import class_schema
 from dataclasses import dataclass
-from typing import Optional, List
-from marshmallow import Schema, fields, validate
+from typing import Optional
 from marshmallow_jsonschema import JSONSchema
+from babel.dates import format_datetime
+from datetime import datetime
 
 
-def get_weather(city_name: str) -> Optional[dict]:
+def get_weather(city_name):
     """
-    Получает данные о погоде для указанного города.
+    Получает данные о погоде для указанного города с помощью API.
 
     Args:
-    city_name: Название города.
+    city_name (str): Название города.
 
     Returns:
     dict: Данные о погоде в формате JSON.
     """
-    api_key = "748a58c619d858a68d8f4441207ea03c"  # Мой API ключ
-    base_url = "https://api.openweathermap.org/data/2.5/weather"
+    api_key = '23496c2a58b99648af590ee8a29c5348'
+    base_url = 'http://api.openweathermap.org/data/2.5/weather'
     params = {
         'q': city_name,
         'lang': 'ru',
@@ -26,66 +28,107 @@ def get_weather(city_name: str) -> Optional[dict]:
         'appid': api_key
     }
     response = requests.get(base_url, params=params)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return None
+    return response.json()
 
 
 @dataclass
 class CurrentWeather:
     """
-    Класс для хранения данных о текущей погоде.
+    Датакласс для хранения данных о текущей погоде.
     """
     city_name: str
-    country: str
     temperature: float
-    feels_like: float
+    description: str
     humidity: int
-    pressure: int
-    weather_description: str
     wind_speed: float
-    wind_direction: int
-    sunrise: int
-    sunset: int
-    snow: Optional[float]
-    rain: Optional[float]
-    visibility: int
-    timezone: int
-    coordinates: dict
+    pressure: int
+    visibility: Optional[int] = None
+    sunrise: Optional[int] = None
+    sunset: Optional[int] = None
 
 
-class CurrentWeatherSchema(Schema):
+WeatherSchema = class_schema(CurrentWeather)
+
+
+class ExtendedWeatherSchema(WeatherSchema):
     """
-    Схема для валидации и сериализации данных о текущей погоде.
+    Расширенная схема.
     """
-    city_name = fields.Str(required=True)
-    country = fields.Str(required=True)
-    temperature = fields.Float(required=True)
-    feels_like = fields.Float(required=True)
-    humidity = fields.Int(required=True, validate=validate.Range(min=0, max=100))
-    pressure = fields.Int(required=True, validate=validate.Range(min=800, max=1200))
-    weather_description = fields.Str(required=True)
-    wind_speed = fields.Float(required=True, validate=validate.Range(min=0))
-    wind_direction = fields.Int(required=True, validate=validate.Range(min=0, max=360))
-    sunrise = fields.Int(required=True)
-    sunset = fields.Int(required=True)
-    snow = fields.Float()
-    rain = fields.Float()
-    visibility = fields.Int(required=True, validate=validate.Range(min=0))
-    timezone = fields.Int(required=True)
-    coordinates = fields.Dict(required=True)
+    temperature = fields.Float(
+        required=True,
+        validate=validate.Range(min=-100, max=100),
+        metadata={'description': 'Температура в градусах Цельсия'}
+    )
+    humidity = fields.Int(
+        required=True,
+        validate=validate.Range(min=0, max=100),
+        metadata={'description': 'Влажность воздуха в процентах'}
+    )
+    wind_speed = fields.Float(
+        required=True,
+        validate=validate.Range(min=0),
+        metadata={'description': 'Скорость ветра в м/с'}
+    )
+    pressure = fields.Int(
+        required=True,
+        validate=validate.Range(min=0),
+        metadata={'description': 'Атмосферное давление в гПа'}
+    )
+    visibility = fields.Int(
+        allow_none=True,
+        validate=validate.Range(min=0),
+        metadata={'description': 'Видимость в метрах'}
+    )
+    sunrise = fields.Int(
+        allow_none=True,
+        metadata={'description': 'Время восхода солнца в UNIX-формате'}
+    )
+    sunset = fields.Int(
+        allow_none=True,
+        metadata={'description': 'Время заката солнца в UNIX-формате'}
+    )
 
 
-if __name__ == "__main__":
-    city = input("Введите, в каком городе хотите узнать погоду: ")
-    weather_data = get_weather(city)
-    if weather_data:
-        pprint.pprint(weather_data)
-        current_weather_schema = CurrentWeatherSchema()
-        json_schema = JSONSchema().dump(current_weather_schema)
-        with open('current_weather_schema.json', 'w') as file:
-            json.dump(json_schema, file, indent=4)
-    else:
-        print("Не удалось получить данные о погоде для указанного города.")
+json_schema = JSONSchema().dump(ExtendedWeatherSchema())
+
+with open("weather_file.json", "w", encoding="utf-8") as file:
+    json.dump(json_schema, file, indent=4, ensure_ascii=False)
+
+city_name_input = input('Введите город для ознакомления с погодой: ')
+weather_data = get_weather(city_name_input)
+
+current_weather = CurrentWeather(
+    city_name=weather_data['name'],
+    temperature=weather_data['main']['temp'],
+    description=weather_data['weather'][0]['description'],
+    humidity=weather_data['main']['humidity'],
+    wind_speed=weather_data['wind']['speed'],
+    pressure=weather_data['main']['pressure'],
+    visibility=weather_data.get('visibility'),
+    sunrise=weather_data.get('sys', {}).get('sunrise'),
+    sunset=weather_data.get('sys', {}).get('sunset')
+)
+
+json_data = ExtendedWeatherSchema().dump(current_weather)
+
+try:
+    validated_data = ExtendedWeatherSchema().load(json_data)
+    print('МЫ НАШЛИ ДЛЯ ВАС ДАННЫЕ !')
+except ValidationError as e:
+    print(f'Ошибка валидации данных: {e.messages}')
+sunrise_time = format_datetime(datetime.fromtimestamp(current_weather.sunrise), format='short', locale='ru')
+sunset_time = format_datetime(datetime.fromtimestamp(current_weather.sunset), format='short', locale='ru')
+
+print(f"Город: {current_weather.city_name}")
+print(f"Температура: {current_weather.temperature}°C")
+print(f"Описание: {current_weather.description}")
+print(f"Влажность: {current_weather.humidity}%")
+print(f"Скорость ветра: {current_weather.wind_speed} м/с")
+print(f"Атмосферное давление: {current_weather.pressure} гПа")
+print(f"Видимость: {current_weather.visibility} м")
+print(f"Время восхода солнца: {sunrise_time}")
+print(f"Время заката солнца: {sunset_time}")
+
+
+
 
